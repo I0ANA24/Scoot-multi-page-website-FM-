559d1f096486350b152acb5dbd8ccc323a8412d6
I0ANA24 <75781040+I0ANA24@users.noreply.github.com>
2024-08-16 11:09:28 +0300

    nav.mobile-only sticky position

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 let hamburger = document.getElementById('hamburger');
 let close = document.getElementById('close');
 let menu = document.querySelector('.menu');
+let hero = document.querySelector('.hero.mobile-only');
 
 hamburger.addEventListener('click', () => {
     hamburger.style.display = "none";
     close.style.display = "block";
     menu.style.display = "flex";
+
+    document.addEventListener('click', (event) => {
+        if(close.style.display === "block" && !menu.contains(event.target) && !hamburger.contains(event.target)) {
+            close.style.display = "none";
+            hamburger.style.display = "block";
+            menu.style.display = "none";
+
+            if(event.target.tagName === 'A') {
+                event.preventDefault();
+            }
+        }
+    });
 });
 
 close.addEventListener('click', () => {
