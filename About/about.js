8c026e288141ae3efae3d5000c8d981814e47a9e
I0ANA24 <75781040+I0ANA24@users.noreply.github.com>
2024-08-17 14:07:29 +0300

    FAQs about page

--- About/about.js
+++ About/about.js
@@ -16,9 +16,23 @@ itemsList.forEach((item) => {
             let chevron = item.children[0];
             setTimeout(() => {
                 answer.style.display = "block";
+                item.style.backgroundColor = "#FFF4DF";
+                answer.style.backgroundColor = "#FFF4DF";
             }, 300);
             chevron.style.transform = "rotate(180deg)";
         }
         console.log(answer);
+    });
+
+    item.addEventListener('mouseover', (event) => {
+        item.style.backgroundColor = "#FFF4DF";
+    });
+
+    item.addEventListener('mouseout', (event) => {
+        let answer = item.children[2];
+        if(answer.style.display === "block") {
+            item.style.backgroundColor = "#FFF4DF";    
+        } else
+            item.style.backgroundColor = "#F2F5F9";
     });
 });
