a26b3e4bdac93b1f94cafe2095c05d368b5a3a6d
I0ANA24 <75781040+I0ANA24@users.noreply.github.com>
2024-08-18 14:54:21 +0300

    no scroll and swipe for menu

--- script.js
+++ script.js
@@ -7,12 +7,14 @@ hamburger.addEventListener('click', () => {
     hamburger.style.display = "none";
     close.style.display = "block";
     menu.style.display = "flex";
+    document.body.style.overflow = "hidden";
 
     document.addEventListener('click', (event) => {
         if(close.style.display === "block" && !menu.contains(event.target) && !hamburger.contains(event.target)) {
             close.style.display = "none";
             hamburger.style.display = "block";
             menu.style.display = "none";
+            document.body.style.overflow = "scroll";
 
             if(event.target.tagName === 'A') {
                 event.preventDefault();
@@ -25,4 +27,33 @@ close.addEventListener('click', () => {
     close.style.display = "none";
     hamburger.style.display = "block";
     menu.style.display = "none";
+    document.body.style.overflow = "scroll";
+});
+
+let startX = 0;
+let endX = 0;
+
+document.addEventListener('touchstart', function(e) {
+    startX = e.touches[0].clientX;
+});
+
+document.addEventListener('touchmove', function(e) {
+    endX = e.touches[0].clientX;
+});
+
+document.addEventListener('touchend', function(e) {
+    let deltaX = endX - startX;
+
+    if (deltaX > 50) { // swipe dreapta
+        hamburger.style.display = "none";
+        close.style.display = "block";
+        menu.style.display = "flex";
+        document.body.style.overflow = "hidden";
+    } else if (deltaX < -50) { // swipe stânga
+        close.style.display = "none";
+        hamburger.style.display = "block";
+        menu.style.display = "none";
+        document.body.style.overflow = "scroll";
+
+    }
 });
